398c5c4a924cbed0b48b75364e16b9330848bd7e
Mill Daulagala <mill.d@wisenet.co>
2021-09-11 19:34:07 +1000

    added team model and wrote logic to find total matches and total wins against each team

--- src/main/java/io/javabrains/ipldashboard/data/MatchDataProcessor.java
+++ src/main/java/io/javabrains/ipldashboard/data/MatchDataProcessor.java
@@ -45,6 +45,7 @@ import org.slf4j.Logger;
 
         match.setTossWinner(matchInput.getToss_winner());
         match.setTossDecision(matchInput.getToss_decision());
+        match.setMatchWinner(matchInput.getWinner());
         match.setResult(matchInput.getResult());
         match.setResultMargin(matchInput.getResult_margin());
         match.setUmpire1(matchInput.getUmpire1());
